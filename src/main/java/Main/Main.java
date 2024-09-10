9945e5172d9bd07d170198a04bed9be1e4aac4b6
NFRamz <133985045+NFRamz@users.noreply.github.com>
2024-09-10 19:54:28 +0700

    adding Exception Handling

--- src/main/java/Main/Main.java
+++ src/main/java/Main/Main.java
@@ -1,8 +1,14 @@
 package Main;
 
+import commands.CMD;
+
 import data.LoginMenu;
 import javafx.application.Application;
 import javafx.stage.Stage;
+
+
+import java.io.IOException;
+import java.util.Arrays;
 
 public class Main extends Application {
 
@@ -10,8 +16,22 @@ public class Main extends Application {
     public void start(Stage primaryStage) {
         LoginMenu loginMenuObj = new LoginMenu();
 
-        loginMenuObj.menu();
+
+            try{
+                CMD.runCommands(CMD.listCommands[0]);
+
+                loginMenuObj.menu();
+
+            }catch (IOException e){
+                System.out.println("Error: Main.java, Method: start\n"+"Details information:\n"+e);
+            }
+
+
+
+
     }
+
+
 
     public static void main(String[] args) {
         launch(args);
