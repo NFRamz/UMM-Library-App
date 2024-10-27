590f071c8186e8978700a53500434423213fb1a1
NFRamz <nframzi051@gmail.com>
2024-10-27 15:02:13 +0700

    add javadoc

--- src/main/java/Main/Main.java
+++ src/main/java/Main/Main.java
@@ -1,3 +1,10 @@
+/**
+ * Class Main.java
+ * This class is the main class of the project.
+ * This class is responsible for starting the application.
+ * This class extends the Application class of the JavaFX library.
+ *
+ */
 package Main;
 
 import commands.CMD;
@@ -8,9 +15,9 @@ import javafx.stage.Stage;
 
 
 import java.io.IOException;
-import java.util.Arrays;
 
 public class Main extends Application {
+
 
     @Override
     public void start(Stage primaryStage) {
